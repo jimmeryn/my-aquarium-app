26418c3464f94685ea922fdf52b455cc836bf7cd
jimmeryn <jimmeryn@gmail.com>
2021-01-05 13:55:44 +0100

    graph feature

---
+++ src/features/graph/graphSlice.ts
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+ type SliceState = {};
+const initialState: SliceState[] = [];
+
+const graphSlice = createSlice({
+  name: "graph",
+  initialState,
+  reducers: {
+      addGraphData() {
+
+      }
+  }
+})
+
+export const { addGraphData } = graphSlice.actions
+
+export default graphSlice.reducer
