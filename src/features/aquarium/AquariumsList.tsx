d6ccc0f130f8218a78471b47ffb7d9fcf5c03602
jimmeryn <jimmeryn@gmail.com>
2021-01-05 13:55:34 +0100

    aquarium feature

---
+++ src/features/aquarium/AquariumsList.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const AquariumList = () => <div></div>;
+
+export default AquariumList;
---
+++ src/features/aquarium/aquariumsSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+ type SliceState = { name?: string, size: number, startDate: Date };
+const initialState: SliceState[] = [];
+
+const aquariumSlice = createSlice({
+  name: "aquarium",
+  initialState,
+  reducers: {
+    addAquarium(state, action: PayloadAction<{ name?: string, size: number, startDate: Date }>) {
+      const { name, size, startDate } = action.payload
+      state.push({ name, size, startDate })
+    }
+  }
+})
+
+export const { addAquarium } = aquariumSlice.actions
+
+export default aquariumSlice.reducer
