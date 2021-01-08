be90ac7ef62f53a707606fe6856bffcedb556723
jimmeryn <jimmeryn@gmail.com>
2021-01-08 22:18:25 +0100

    navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import * as React from "react";
+import styled from "styled-components";
+// import Hamburger from "/Hamburger";
 
-const Navbar = () => <div>Navbar</div>;
+const StyledNav = styled.div`
+  background-color: #009be5;
+  min-height: 7vh;
+  height: 7vh;
+  width: 81vw;
+  display: flex;
+  box-shadow: 0 2px 3px #0000003a;
+  position: sticky;
+  top: 0;
+`;
+
+const Navbar: React.FunctionComponent = () => (
+  <StyledNav>{/* <Hamburger /> */}</StyledNav>
+);
 
 export default Navbar;
