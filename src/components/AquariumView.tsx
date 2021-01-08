63e50d1e08321b9cc2a2ad86f8bc20dde70b8ed0
jimmeryn <jimmeryn@gmail.com>
2021-01-08 22:15:38 +0100

    renamed

--- src/components/AquariumView.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AquariumView: React.FunctionComponent<{ name: string }> = ({ name }) => (
-  <div>{`AquariumView ${name}`}</div>
-);
-
-export default AquariumView;
