7952688843c619fc53df80b8646274e3b6a6d713
jimmeryn <jimmeryn@gmail.com>
2021-01-08 22:30:09 +0100

    typography styling

---
+++ src/components/Typography.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  margin: 0;
+  font-family: "Poppins";
+  font-style: normal;
+  text-align: right;
+  margin-left: 0;
+  font-weight: 10;
+  font-size: 36px;
+  line-height: 40px;
+  text-transform: capitalize;
+  color: #000000;
+`;
+
+export const Subtitle = styled(Title)`
+  margin-left: 150;
+  font-weight: 200;
+  font-size: 24px;
+  line-height: 24px;
+  text-transform: lowercase;
+  color: #757575;
+`;
