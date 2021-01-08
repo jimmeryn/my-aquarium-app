e13ae9d81232af25013c6c208c0effb946e2c7d9
jimmeryn <jimmeryn@gmail.com>
2021-01-08 22:19:50 +0100

    basic menu styling

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+import Button from "./buttons/Button";
 
-const Menu = () => <div></div>;
+const StyledMenu = styled.div`
+  background-color: #18202c;
+  height: 100vh;
+  width: 19vw;
+  z-index: 3;
+  overflow-y: auto;
+`;
+
+const Menu: React.FunctionComponent = ({ children }) => (
+  <StyledMenu>
+    {children}
+  </StyledMenu>
+);
 
 export default Menu;
