a75989fb9351ae732ebdc4d844492affbb2fbd39
jimmeryn <jimmeryn@gmail.com>
2021-01-11 19:09:43 +0100

    aquarium param button added

---
+++ src/components/buttons/AquariumParamButton.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { StyledButton } from "./Button";
+
+export const AquariumParamButton = styled(StyledButton)`
+  font-weight: lighter;
+  font-size: 0.9rem;
+  color: white;
+  background-color: #18202c;
+  :hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+`;
