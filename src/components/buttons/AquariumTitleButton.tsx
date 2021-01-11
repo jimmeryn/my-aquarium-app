e91559960ec634b1e97792b4190059d494d93546
jimmeryn <jimmeryn@gmail.com>
2021-01-11 19:10:05 +0100

    aquarium title button added

---
+++ src/components/buttons/AquariumTitleButton.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { StyledButton } from "./Button";
+
+export const AquariumTitleButton = styled(StyledButton)`
+  color: #bdc1b7;
+  font-size: 0.95rem;
+  font-weight: normal;
+  background-color: #232f3e;
+  border-top: 2px solid #313b49;
+  border-bottom: 2px solid #313b49;
+`;
