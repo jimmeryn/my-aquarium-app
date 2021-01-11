5d299037fcb6b2be3155df5231f27048da2907db
jimmeryn <jimmeryn@gmail.com>
2021-01-11 19:09:22 +0100

    basic button styling

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,25 +1,41 @@
 import React from "react";
-import Divider from "./Divider";
+import styled, { css } from "styled-components";
 
 const Button: React.FunctionComponent<{
+  className: string;
   name: string;
-  dividers?: boolean;
   handleButtonClick?: () => void;
-}> = ({ name, dividers = false, handleButtonClick }) => (
-  <React.Fragment>
-    {dividers ? <Divider /> : null}
-    <div>
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          if (handleButtonClick) handleButtonClick();
-        }}
-      >
-        <div>{name}</div>
-      </button>
-    </div>
-    {dividers ? <Divider /> : null}
-  </React.Fragment>
+}> = ({ className, name, handleButtonClick }) => (
+  <button
+    className={className}
+    onClick={(e) => {
+      e.preventDefault();
+      if (handleButtonClick) handleButtonClick();
+    }}
+  >
+    {name}
+  </button>
 );
 
-export default Button;
+export const StyledButton = styled(Button)<{ dividers?: boolean }>`
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  height: 7vh;
+  font-family: "Poppins";
+  text-transform: uppercase;
+  cursor: pointer;
+  color: white;
+
+  opacity: 1;
+  :hover {
+    opacity: 0.8;
+  }
+  border: none;
+  ${(props) =>
+    props.dividers &&
+    css`
+      border-top: 2px solid #313b49;
+      border-bottom: 2px solid #313b49;
+    `}
+`;
