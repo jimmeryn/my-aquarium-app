8a89e3d453d2d031924689ddc8ae5e5dc6df7d76
jimmeryn <jimmeryn@gmail.com>
2021-01-11 19:09:55 +0100

    title button added

---
+++ src/components/buttons/TitleButton.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { StyledButton } from "./Button";
+
+export const TitleButton = styled(StyledButton)`
+  color: #bdc1b7;
+  background-color: #232f3e;
+  font-weight: bold;
+  font-size: 1.04rem;
+`;
