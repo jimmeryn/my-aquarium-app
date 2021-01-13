9d115f99592896ceb2700afd53c4577b6dbbd326
jimmeryn <jimmeryn@gmail.com>
2021-01-13 15:39:32 +0100

    basic aquarium page setup

--- src/components/pages/AquariumPage.tsx
+++ src/components/pages/AquariumPage.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import Page from "./Page";
 
-const AquariumPage: React.FunctionComponent = () => {
-  return <Page></Page>;
-};
+import Page from "./Page";
+import PageContent from "./PageContent";
+import Menu from "components/Menu";
+import Navbar from "components/Navbar";
+
+const AquariumPage: React.FunctionComponent = () => (
+  <Page className="page">
+    <Menu className="menu"></Menu>
+    <PageContent>
+      <Navbar className="navbar" />
+    </PageContent>
+  </Page>
+);
 
 export default AquariumPage;
