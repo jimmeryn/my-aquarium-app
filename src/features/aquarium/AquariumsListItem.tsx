581cede550fdbc37d38e6baffc0fa8027bb51464
jimmeryn <jimmeryn@gmail.com>
2021-01-13 16:16:42 +0100

    created AquariumListItem

--- src/features/aquarium/AquariumsListItem.tsx
+++ src/features/aquarium/AquariumsListItem.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { AquariumParamButton } from "components/buttons/AquariumParamButton";
+import { AquariumTitleButton } from "components/buttons/AquariumTitleButton";
+import { Link } from "react-router-dom";
 
-const AquariumListItem = () => <div></div>;
+const AquariumsListItem: React.FunctionComponent<{
+  id: number;
+  name?: string;
+}> = ({ id, name }) => (
+  <React.Fragment key={id}>
+    <Link to={`/aquarium/${id}`}>
+      <AquariumTitleButton
+        className="aquarium-title"
+        name={name && name.length > 0 ? name : `Aquarium #${id + 1}`}
+      />
+    </Link>
+    <Link to={`/aquarium/${id}`}>
+      <AquariumParamButton
+        className="aquarium-param-button"
+        name="Show Params"
+      />
+    </Link>
+    <AquariumParamButton className="aquarium-param-button" name="Add Params" />
+    <AquariumParamButton className="aquarium-param-button" name="Add Refill" />
+  </React.Fragment>
+);
 
-export default AquariumListItem;
+export default AquariumsListItem;
