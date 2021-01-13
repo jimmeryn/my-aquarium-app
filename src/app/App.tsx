dad1881b0b343e53f8befa71218135b2a4cf3709
jimmeryn <jimmeryn@gmail.com>
2021-01-13 15:52:23 +0100

    date require outside of component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,27 +5,25 @@ import Home from "components/pages/Home";
 import AquariumPage from "components/pages/AquariumPage";
 import { Aquarium } from "models";
 
-const App: React.FunctionComponent = () => {
-  const aquariums: Aquarium[] = require("api/db.json").aquariums;
+const aquariums: Aquarium[] = require("api/db.json").aquariums;
 
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Home aquariums={aquariums} />
+const App: React.FunctionComponent = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <Home aquariums={aquariums} />
+      </Route>
+      {aquariums.map(({ id }) => (
+        <Route key={id} path={`/aquarium/${id}`}>
+          <AquariumPage />
         </Route>
-        {aquariums.map(({ id }) => (
-          <Route key={id} path={`/aquarium/${id}`}>
-            <AquariumPage />
-          </Route>
-        ))}
-        <Route path="*">
-          {/* TODO: Error page 404*/}
-          <Home aquariums={aquariums} />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
-};
+      ))}
+      <Route path="*">
+        {/* TODO: Error page 404*/}
+        <Home aquariums={aquariums} />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
