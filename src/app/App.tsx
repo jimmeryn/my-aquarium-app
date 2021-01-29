8ec7d0768f8edefe1ff58d07b06c63f6541ac988
jimmeryn <jimmeryn@gmail.com>
2021-01-29 16:42:08 +0100

    input TEST props

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,7 +15,53 @@ const App: React.FunctionComponent = () => (
       </Route>
       {aquariums.map(({ id }) => (
         <Route key={id} path={`/aquarium/${id}`}>
-          <AquariumPage />
+          <AquariumPage
+            name={`Aquarium #${id + 1}`}
+            params={[
+              {
+                id: 1,
+                name: "Cl2",
+                value: 0,
+                aquariumId: 0,
+                date: new Date(),
+              },
+              {
+                id: 2,
+                name: "pH",
+                value: 6.2,
+                aquariumId: 0,
+                date: new Date(),
+              },
+              {
+                id: 3,
+                name: "KH",
+                value: 4,
+                aquariumId: 0,
+                date: new Date(),
+              },
+              {
+                id: 4,
+                name: "GH",
+                value: 11,
+                aquariumId: 0,
+                date: new Date(),
+              },
+              {
+                id: 5,
+                name: "NO2",
+                value: 0.1,
+                aquariumId: 0,
+                date: new Date(),
+              },
+              {
+                id: 6,
+                name: "NO3",
+                value: 150,
+                aquariumId: 0,
+                date: new Date(),
+              },
+            ]}
+          />
         </Route>
       ))}
       <Route path="*">
