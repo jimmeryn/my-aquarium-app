115bfdb2b009bd0b1c1703fa9183267194f39ab9
jimmeryn <jimmeryn@gmail.com>
2021-01-31 14:12:54 +0100

    get data from redux

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,73 +3,32 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Home from "components/pages/Home";
 import AquariumPage from "components/pages/AquariumPage";
-import { Aquarium } from "models";
+import { useSelector } from "react-redux";
+import { RootState } from "./rootReducer";
 
-const aquariums: Aquarium[] = require("api/db.json").aquariums;
-
-const App: React.FunctionComponent = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/">
-        <Home aquariums={aquariums} />
-      </Route>
-      {aquariums.map(({ id }) => (
-        <Route key={id} path={`/aquarium/${id}`}>
-          <AquariumPage
-            name={`Aquarium #${id + 1}`}
-            params={[
-              {
-                id: 1,
-                name: "Cl2",
-                value: 0,
-                aquariumId: 0,
-                date: new Date(),
-              },
-              {
-                id: 2,
-                name: "pH",
-                value: 6.2,
-                aquariumId: 0,
-                date: new Date(),
-              },
-              {
-                id: 3,
-                name: "KH",
-                value: 4,
-                aquariumId: 0,
-                date: new Date(),
-              },
-              {
-                id: 4,
-                name: "GH",
-                value: 11,
-                aquariumId: 0,
-                date: new Date(),
-              },
-              {
-                id: 5,
-                name: "NO2",
-                value: 0.1,
-                aquariumId: 0,
-                date: new Date(),
-              },
-              {
-                id: 6,
-                name: "NO3",
-                value: 150,
-                aquariumId: 0,
-                date: new Date(),
-              },
-            ]}
-          />
+const App: React.FunctionComponent = () => {
+  const aquariums = useSelector((state: RootState) => state.aquariumSlice);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home aquariums={aquariums} />
         </Route>
-      ))}
-      <Route path="*">
-        {/* TODO: Error page 404*/}
-        <Home aquariums={aquariums} />
-      </Route>
-    </Switch>
-  </BrowserRouter>
-);
+        {aquariums.map(({ id }, i) => (
+          <Route key={id} path={`/aquarium/${id}`}>
+            <AquariumPage
+              name={`Aquarium #${i + 1}`}
+              aquariumId={id as number}
+            />
+          </Route>
+        ))}
+        <Route path="*">
+          {/* TODO: Error page 404*/}
+          <Home aquariums={aquariums} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
