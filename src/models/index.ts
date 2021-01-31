8da7e243ff1211bdcbb726bb558ca5dd21d3f632
jimmeryn <jimmeryn@gmail.com>
2021-01-31 14:12:17 +0100

    update models to match api schema (todo: id fix)

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,42 +1,15 @@
-export interface UnnormalizedStateData {
-    aquariums: UnnormalizedAquarium[];
-  }
-  
-  export interface UnnormalizedAquarium {
-    id: number;
-    size: number;
-    data: UnnormalizedParam[];
-    waterRefills: UnnormalizedParam[];
-  }
-  
-  export interface UnnormalizedParam {
-    date: string;
-    [key: string]: string | number;
-  }
-  
-  export interface Aquarium {
-    id: number;
-    name?: string;
-    size: number;
-    params: number[];
-    startDate: Date;
-  }
-  
-  export interface Param {
-    id: number;
-    aquariumId: number;
-    date: Date;
-    value: number | null;
-    name: string;
-  }
-  
-  export interface NormalizedObject<T> {
-    byId: { [id: number]: T };
-    allIds: number[];
-  }
-  
-  export interface State {
-    aquariums: NormalizedObject<Aquarium>;
-    params: NormalizedObject<Param>;
-    visibleAquarium: number;
-  }
+export interface Aquarium {
+  id?: number;
+  name?: string;
+  size: number;
+  params: Param[];
+  startDate: Date;
+}
+
+export interface Param {
+  id?: number;
+  date: Date;
+  name: string;
+  value: number | null;
+  aquariumId: number;
+}
