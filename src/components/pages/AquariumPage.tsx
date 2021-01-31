5097f6d37c1030f6fda32589a27750671284ba84
jimmeryn <jimmeryn@gmail.com>
2021-01-31 14:14:12 +0100

    TEST filter params and refilValue from all params

--- src/components/pages/AquariumPage.tsx
+++ src/components/pages/AquariumPage.tsx
@@ -6,32 +6,54 @@ import Menu from "components/Menu";
 import Navbar from "components/Navbar";
 import LatestParamList from "features/param/LatestParamsList";
 import { AquariumTitleButton } from "components/buttons/AquariumTitleButton";
-import { Param } from "models";
 import AddRefillButton from "features/param/AddRefillButton";
 import AddParamsButton from "features/param/AddParamsButton";
 import ParamTable from "features/param/ParamsTable";
 import Graph from "features/graph/Graph";
+import { useSelector } from "react-redux";
+import { RootState } from "app/rootReducer";
+import { Param } from "models";
 
 const AquariumPage: React.FunctionComponent<{
   name: string;
-  params: Param[];
-  refillValue?: number;
-}> = ({ name, params, refillValue }) => (
-  <Page className="page">
-    <Menu className="menu">
-      <AquariumTitleButton className="aquarium-title">
-        {name}
-      </AquariumTitleButton>
-      <AddRefillButton refillValue={refillValue} />
-      <AddParamsButton />
-      <LatestParamList params={params} />
-    </Menu>
-    <PageContent>
-      <Navbar className="navbar" />
-      <Graph />
-      <ParamTable />
-    </PageContent>
-  </Page>
-);
+  aquariumId: number;
+}> = ({ name, aquariumId }) => {
+  function getLatestParamsAndFilterValue(params: Param[]) {
+    let initState: { params: Param[]; refillValue: number | undefined } = {
+      params: [],
+      refillValue: undefined,
+    };
+
+    return params.reduce(
+      (paramAndFilterObject, currentParam) =>
+        currentParam.name === "refill"
+          ? { ...paramAndFilterObject }
+          : { ...paramAndFilterObject, params: [...params, currentParam] },
+      initState
+    );
+  }
+
+  const { params, refillValue } = useSelector((state: RootState) =>
+    getLatestParamsAndFilterValue(state.aquariumSlice[aquariumId].params)
+  );
+
+  return (
+    <Page className="page">
+      <Menu className="menu">
+        <AquariumTitleButton className="aquarium-title">
+          {name}
+        </AquariumTitleButton>
+        <AddRefillButton refillValue={refillValue} />
+        <AddParamsButton />
+        <LatestParamList params={params} />
+      </Menu>
+      <PageContent>
+        <Navbar className="navbar" />
+        <Graph />
+        <ParamTable />
+      </PageContent>
+    </Page>
+  );
+};
 
 export default AquariumPage;
