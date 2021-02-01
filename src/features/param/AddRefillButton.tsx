a3c7ffd2af8e924016ac226f28448a2e0015493d
jimmeryn <jimmeryn@gmail.com>
2021-02-01 13:40:38 +0100

    created container - button gets own data

--- src/features/param/AddRefillButton.tsx
+++ src/features/param/AddRefillButton.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "app/rootReducer";
 import AddButton from "components/buttons/AddButton";
+import { getLatestRefillValue } from "api/filterParams";
 
-const AddRefillButton: React.FunctionComponent<{
-  refillValue?: number;
-}> = ({ refillValue }) => (
-  <AddButton
-    className="add-refill-button"
-    name="Refill"
-    value={refillValue ?? "No refills"}
-  />
-);
+const AddRefillButton: React.FunctionComponent<{ aquariumId: number }> = ({
+  aquariumId,
+}) => {
+  const refillValue = useSelector((state: RootState) =>
+    getLatestRefillValue(state.aquariumSlice[aquariumId].params)
+  );
+  return (
+    <AddButton
+      className="add-refill-button"
+      name="Refill"
+      value={refillValue ?? "No refills"}
+    />
+  );
+};
 
 export default AddRefillButton;
