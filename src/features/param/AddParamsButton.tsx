6d664bf69cb0cb24b83ab88dccbb6c03386f0605
jimmeryn <jimmeryn@gmail.com>
2021-02-03 15:51:25 +0100

    add param button with state

--- src/features/param/AddParamsButton.tsx
+++ src/features/param/AddParamsButton.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import AddButton from "components/buttons/AddButton";
+import { useDispatch } from "react-redux";
+import { DialogType, openDialog } from "features/dialog/dialogSlice";
 
-const AddParamsButton: React.FunctionComponent = () => (
-  <AddButton className="add-params-button" name="Parameters" />
-);
+const AddParamsButton: React.FunctionComponent = () => {
+  const dispatch = useDispatch();
+  return (
+    <AddButton
+      className="add-params-button"
+      name="Parameters"
+      handleOnClick={() => dispatch(openDialog({ type: DialogType.addParam }))}
+    />
+  );
+};
 
 export default AddParamsButton;
