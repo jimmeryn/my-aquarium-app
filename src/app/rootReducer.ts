e73244f76341791ef61897472fd4004b1ec19260
jimmeryn <jimmeryn@gmail.com>
2021-02-03 15:48:39 +0100

    added dialog and graph slices

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,11 +1,15 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import paramSlice from 'features/param/paramsSlice';
 import aquariumSlice from 'features/aquarium/aquariumsSlice';
+import dialogSlice from 'features/dialog/dialogSlice';
+import graphSlice from 'features/graph/graphSlice';
 
 
 const rootReducer = combineReducers({
   paramSlice,
-  aquariumSlice
+  aquariumSlice,
+  dialogSlice,
+  graphSlice
 })
 
 export type RootState = ReturnType<typeof rootReducer>
