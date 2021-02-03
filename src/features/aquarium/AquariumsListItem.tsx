3cb62c1bdabaaef8048fc79f316793a8ed82bd08
jimmeryn <jimmeryn@gmail.com>
2021-02-03 15:56:58 +0100

    made container

--- src/features/aquarium/AquariumsListItem.tsx
+++ src/features/aquarium/AquariumsListItem.tsx
@@ -1,30 +1,45 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { AquariumParamButton } from "components/buttons/AquariumParamButton";
 import { AquariumTitleButton } from "components/buttons/AquariumTitleButton";
-import { Link } from "react-router-dom";
+import { DialogType, openDialog } from "features/dialog/dialogSlice";
 
 const AquariumsListItem: React.FunctionComponent<{
   id: number;
   name?: string;
-}> = ({ id, name }) => (
-  <React.Fragment key={id}>
-    <Link to={`/aquarium/${id}`}>
-      <AquariumTitleButton className="aquarium-title">
-        {name && name.length > 0 ? name : `Aquarium #${id + 1}`}
-      </AquariumTitleButton>
-    </Link>
-    <Link to={`/aquarium/${id}`}>
-      <AquariumParamButton className="aquarium-param-button">
-        {"Show Params"}
+}> = ({ id, name }) => {
+  const dispatch = useDispatch();
+  return (
+    <React.Fragment key={id}>
+      <Link to={`/aquarium/${id}`}>
+        <AquariumTitleButton className="aquarium-title">
+          {name && name.length > 0 ? name : `Aquarium #${id + 1}`}
+        </AquariumTitleButton>
+      </Link>
+      <Link to={`/aquarium/${id}`}>
+        <AquariumParamButton className="aquarium-param-button">
+          {"Show Params"}
+        </AquariumParamButton>
+      </Link>
+      <AquariumParamButton
+        className="aquarium-param-button"
+        handleOnClick={() =>
+          dispatch(openDialog({ type: DialogType.addParam }))
+        }
+      >
+        {"Add Params"}
       </AquariumParamButton>
-    </Link>
-    <AquariumParamButton className="aquarium-param-button">
-      {"Add Params"}
-    </AquariumParamButton>
-    <AquariumParamButton className="aquarium-param-button">
-      {"Add Refill"}
-    </AquariumParamButton>
-  </React.Fragment>
-);
+      <AquariumParamButton
+        className="aquarium-param-button"
+        handleOnClick={() =>
+          dispatch(openDialog({ type: DialogType.addRefill }))
+        }
+      >
+        {"Add Refill"}
+      </AquariumParamButton>
+    </React.Fragment>
+  );
+};
 
 export default AquariumsListItem;
--- src/features/param/AddRefillButton.tsx
+++ src/features/param/AddRefillButton.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "app/rootReducer";
 import { getLatestRefillValue } from "api/filterParamsFunctions";
 import AddButton from "components/buttons/AddButton";
+import { DialogType, openDialog } from "features/dialog/dialogSlice";
 
 const AddRefillButton: React.FunctionComponent<{ aquariumId: number }> = ({
   aquariumId,
@@ -10,12 +11,14 @@ const AddRefillButton: React.FunctionComponent<{ aquariumId: number }> = ({
   const refillValue = useSelector((state: RootState) =>
     getLatestRefillValue(state.aquariumSlice[aquariumId].params)
   );
+  const dispatch = useDispatch();
 
   return (
     <AddButton
       className="add-refill-button"
       name="Refill"
       value={refillValue ?? "No refills"}
+      handleOnClick={() => dispatch(openDialog({ type: DialogType.addRefill }))}
     />
   );
 };
