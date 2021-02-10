81b136474bc0fd378e882c3cb441874b23975fa5
jimmeryn <jimmeryn@gmail.com>
2021-02-10 15:48:29 +0100

    deleted paramSlice

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,11 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import paramSlice from "features/param/paramsSlice";
 import aquariumSlice from "features/aquarium/aquariumsSlice";
 import dialogSlice from "features/dialog/dialogSlice";
 import graphSlice from "features/graph/graphSlice";
 
 const rootReducer = combineReducers({
-  paramSlice,
   aquariumSlice,
   dialogSlice,
   graphSlice,
